376dbe15878d4a1dd43b949e2c3ba1d22a3407b1
aldousclement <165456036+aldousclement@users.noreply.github.com>
2024-03-30 14:17:01 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        for (int i = 1; i <= 100; i++)
+        {
+            string output = (i % 3 == 0 && i % 5 == 0) ? "Hello Goodbye" :
+                    (i % 3 == 0) ? "Hello" :
+                    (i % 5 == 0) ? "GoodBye" :
+                    i.ToString();
+                    Console.WriteLine(output);
+        }
+    }
+}
